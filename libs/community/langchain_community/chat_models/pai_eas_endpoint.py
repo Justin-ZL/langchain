337fe6528bb55492dd036f252c63ef95b672db1f
Your Name <you@example.com>
2024-05-13 12:41:35 +0000

    Fix return type issue in PaiEasChatEndpoint._call_eas

--- libs/community/langchain_community/chat_models/pai_eas_endpoint.py
+++ libs/community/langchain_community/chat_models/pai_eas_endpoint.py
@@ -208,6 +208,8 @@ class PaiEasChatEndpoint(BaseChatModel):
 
         return generated_text
 
+
+
     def _call_eas(self, query_body: dict) -> Any:
         """Generate text from the eas service."""
         headers = {
@@ -215,19 +217,15 @@ class PaiEasChatEndpoint(BaseChatModel):
             "Accept": "application/json",
             "Authorization": f"{self.eas_service_token}",
         }
-
-        # make request
         response = requests.post(
             self.eas_service_url, headers=headers, json=query_body, timeout=self.timeout
         )
-
         if response.status_code != 200:
             raise Exception(
                 f"Request failed with status code {response.status_code}"
                 f" and message {response.text}"
             )
-
-        return response.text
+        return response.content
 
     def _call_eas_stream(self, query_body: dict) -> Any:
         """Generate text from the eas service."""
